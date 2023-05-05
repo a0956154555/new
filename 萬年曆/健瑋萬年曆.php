4bb6d2946e55fc1daa17493e2260a1076883e3ca
a0956154555 <wayne870726@yahoo.com.tw>
2023-05-05 14:51:26 +0800

    1

--- 萬年曆/健瑋萬年曆.php
+++ 萬年曆/健瑋萬年曆.php
@@ -75,46 +75,62 @@
 <body>
     <?php
     $cyear = $_GET["cyear"];
+
     // $cweight = $_GET["cweight"];
     // $cheight = $_GET["cheight"];
     // $bmi = number_format($cweight / ($cheight * $cheight), 1);
     // echo $cyear;
     // 要先明白閏年的規則 4年一潤 100年不潤 400年在潤一次
-    $February = 29;
-    $count =  $cyear % 400;
-    $day = 365;
-    if ($count % 4 == 0 && $count != 100) {
-        $day = 366;
-    } elseif ($count % 4 != 0) {
-        $day = 365;
-    };
-    // 2022 星期六 2023星期日 2024星期二
 
+    /*
+其表達式爲：1582年10月15日及以後用w=[y+(y/4)+(c/4)-2*c+(26*(m+1)/10)+d-1]（mod7）來計算；1582年10月4日及以前用w=[y+（y/4）+c/4-2*c+13*(m+1)/5+d+2]（mod7）來計算。
+這裡的y爲年份的後兩位數、c爲年份的前兩位數、m爲月數（取值範圍爲3-14，每年的1月和2月視爲13和14）、d爲日序數、mod7代表除以7的正餘數。需要指出的是，當年份前或者後兩位數、月份值應用上式除以相應數值時
+，要向下取整，確保商與除數之積不大於被除數。最後w取值如果爲0，則代表星期日；值爲1，代表星期一，就此類推。
+    */
+
+    /*
+    W= (d+2*m+3*(m+1)/5+y+y/4-y/100+y/400)%7
+
+原文網址：https://kknews.cc/news/avlkjl6.html
+    */
+    /*
+    $test = ($lastDate + 2 * $lastMonth + 3 * ($lastMonth + 1) / 5 + $y + floor($y / 4) - floor($c / 4) - 2 * $c) % 7;
+    */
+    // $lastMonth = 12;
+    // $lastDate = 31;
+    // $y = substr($cyear, 2);
+    // $c = substr($cyear, 0, 2);
+
+    // $test = ($lastDate + 2 * $lastMonth + 3 * ($lastMonth + 1) / 5 + $y + floor($y / 4) - floor($c / 4) - 2 * $c) % 7;
+    // echo $cyear . "的最後一天是" . $test;
+
+
+    // 計算星期幾
+    $cyearCount = $cyear - 1;
+    $lastMonth = 12;
+    $lastDate = 31;
+    $test = (($lastDate + 2 * $lastMonth + 3 * ($lastMonth + 1) / 5 + $cyearCount + floor($cyearCount / 4) - floor($cyearCount / 100) + floor($cyearCount / 400)) % 7) + 1;
+
+
+    echo "{$cyearCount}年12月31日是星期{$test}";
+    $february = 28;
     if ($cyear % 4 == 0) {
-
-        // 取得所有4的倍數
-        if ($cyear % 100 == 0 && $cyear % 400 != 0) {
-            // 肯定是閏年
-            $February = 28;
-            echo "$cyear 年不是閏年";
-        } elseif ($cyear < 100 && $cyear % 4 == 0) {
-            $February = 29;
-            echo "$cyear 年是閏年";
+        if (($cyear % 100 == 0 && $cyear % 400 == 0) || ($cyear % 100 != 0 && $cyear % 4 == 0)) {
+            $february = 29;
+            // echo "$cyear 年是閏年";
         } else {
-            if ($cyear % 400 == 0) {
-                $February = 29;
-                echo "$cyear 年是閏年";
-            }
+            $february = 28;
+            // echo "$cyear 年不是閏年";
         }
     } else {
-        $February = 28;
-        echo "$cyear 不是閏年";
+        $february = 28;
+        // echo "$cyear 不是閏年";
     }
 
     ?>
     <?php
-    $month = array("一月" => 31, "二月" => $February, "三月" => 31, "四月" => 30, "五月" => 31, "六月" => 30, "七月" => 31, "八月" => 31, "九月" => 30, "十月" => 31, "十一月" => 30, "十二月" => 31);
-    $moveDay = 0;
+    $month = array("一月" => 31, "二月" => $february, "三月" => 31, "四月" => 30, "五月" => 31, "六月" => 30, "七月" => 31, "八月" => 31, "九月" => 30, "十月" => 31, "十一月" => 30, "十二月" => 31);
+    $moveDay = ($test % 7) + 1;
     ?>
     <?php foreach ($month as $index => $item) {
 
